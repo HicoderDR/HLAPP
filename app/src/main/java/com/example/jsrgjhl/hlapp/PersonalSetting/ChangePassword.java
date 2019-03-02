7c025d656ca002b7ee476849aff753b29c1da9d5
xuxiaojin <1319039722@qq.com>
2019-03-02 23:37:57 +0800

    个人账户信息页面
    还差一个pickview

--- app/src/main/java/com/example/jsrgjhl/hlapp/PersonalSetting/ChangePassword.java
+++ app/src/main/java/com/example/jsrgjhl/hlapp/PersonalSetting/ChangePassword.java
@@ -1,24 +1,22 @@
 package com.example.jsrgjhl.hlapp.PersonalSetting;
 
-import android.app.Person;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.jsrgjhl.hlapp.Activity.SolveActivity;
 import com.example.jsrgjhl.hlapp.R;
 
-public class PersonalAccount extends AppCompatActivity {
+public class ChangePassword extends AppCompatActivity {
 
-    private TextView changePasswordTv;
-    private TextView areaDefenseTv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_personal_account);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.personalAcountBar);
+        setContentView(R.layout.activity_change_password);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.changePasswordBar);
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
         if (getSupportActionBar() != null) {
@@ -31,22 +29,12 @@ public class PersonalAccount extends AppCompatActivity {
                 finish();
             }
         });
-        areaDefenseTv=(TextView)findViewById(R.id.areaDefense);
-        areaDefenseTv.setOnClickListener(new View.OnClickListener() {
+        Button confirmButton=(Button)findViewById(R.id.confirm_Btn);
+        confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Toast.makeText(ChangePassword.this,"修改成功", Toast.LENGTH_SHORT).show();
             }
         });
-        //修改密码点击事件
-        changePasswordTv=(TextView)findViewById(R.id.changepassword);
-        changePasswordTv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(PersonalAccount.this,ChangePassword.class);
-                startActivity(intent);
-            }
-        });
-
     }
 }
