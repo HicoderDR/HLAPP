bb6f0e0a53f44ebc70a03b572bb5f08392edf572
xuxiaojin <1319039722@qq.com>
2019-03-03 11:50:55 +0800

    区域防区pickview更改

--- app/src/main/java/com/example/jsrgjhl/hlapp/PersonalSetting/PersonalAccount.java
+++ app/src/main/java/com/example/jsrgjhl/hlapp/PersonalSetting/PersonalAccount.java
@@ -7,17 +7,30 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bigkoo.pickerview.builder.OptionsPickerBuilder;
+import com.bigkoo.pickerview.listener.OnOptionsSelectChangeListener;
+import com.bigkoo.pickerview.listener.OnOptionsSelectListener;
+import com.bigkoo.pickerview.view.OptionsPickerView;
 import com.example.jsrgjhl.hlapp.R;
+
+import java.util.ArrayList;
 
 public class PersonalAccount extends AppCompatActivity {
 
     private TextView changePasswordTv;
     private TextView areaDefenseTv;
+    private ArrayList<String> areas = new ArrayList<>();
+    private ArrayList<String> defenses = new ArrayList<>();
+    private OptionsPickerView pvNoLinkOptions;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_account);
+        getOptionData();
+        initNoLinkOptionsPicker();
         Toolbar toolbar = (Toolbar) findViewById(R.id.personalAcountBar);
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
@@ -35,7 +48,8 @@ public class PersonalAccount extends AppCompatActivity {
         areaDefenseTv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                if(pvNoLinkOptions!=null)
+                    pvNoLinkOptions.show();
             }
         });
         //修改密码点击事件
@@ -47,6 +61,51 @@ public class PersonalAccount extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        //
 
     }
+
+    private void initNoLinkOptionsPicker() {// 不联动的多级选项
+        pvNoLinkOptions = new OptionsPickerBuilder(this, new OnOptionsSelectListener() {
+
+            @Override
+            public void onOptionsSelect(int options1, int options2, int options3, View v) {
+
+                String str =  areas.get(options1)
+                        + "/" + defenses.get(options2);
+                areaDefenseTv.setText(str);
+            }
+        })
+                .setTitleText("区域/防区选择")
+                .setLabels("区域", "防区", null)//设置选择的三级单位
+                .setOptionsSelectChangeListener(new OnOptionsSelectChangeListener() {
+                    @Override
+                    public void onOptionsSelectChanged(int options1, int options2, int options3) {
+                        String str = "options1: " + options1 + "\noptions2: " + options2 ;
+                    }
+                })
+                // .setSelectOptions(0, 1, 1)
+                .build();
+        pvNoLinkOptions.setNPicker(areas, defenses, null);
+        pvNoLinkOptions.setSelectOptions(0, 0);
+
+    }
+    private void getOptionData() {
+        /**
+         * 注意：如果是添加JavaBean实体数据，则实体类需要实现 IPickerViewData 接口，
+         * PickerView会通过getPickerViewText方法获取字符串显示出来。
+         */
+        getNoLinkData();
+    }
+
+    private void getNoLinkData() {
+        areas.add("1");
+        areas.add("2");
+        areas.add("3");
+
+        defenses.add("1");
+        defenses.add("2");
+        defenses.add("3");
+    }
+
 }
